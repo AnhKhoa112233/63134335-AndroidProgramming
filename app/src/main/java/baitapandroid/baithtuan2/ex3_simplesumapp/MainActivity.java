3ffd48d7721d5cc9d6cd972f30027fd1b9ff7415
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-03-07 12:32:13 +0700

    bo sung

--- app/src/main/java/baitapandroid/baithtuan2/ex3_simplesumapp/MainActivity.java
+++ app/src/main/java/baitapandroid/baithtuan2/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package baitapandroid.baithtuan2.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void congHaiSo(View view){
+       EditText editTextSoA= findViewById(R.id.so1);
+        EditText editTextSoB = findViewById(R.id.so2);
+       EditText editTextKq= findViewById(R.id.edt_kq);
+        //Lay su lieu cua a ve ve
+       String strA= editTextSoA.getText().toString();
+        //Lay su lieu cua b ve ve
+        String strB= editTextSoB.getText().toString();
+        //Chuyen kieu dl
+       int so_A= Integer.parseInt(strA);
+        int so_B=Integer.parseInt(strB);
+        //Tinh toan
+        int tong=so_A + so_B;
+        String Str_Kq= String.valueOf(tong); //chuyen kq qua dang chuoi
+        //Hien ra man hinh
+        editTextKq.setText(Str_Kq);
     }
 }
