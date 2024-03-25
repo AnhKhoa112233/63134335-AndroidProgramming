9475efe3b631e91f38857aa3b73db974c4edede8
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-03-25 12:15:29 +0700

    Update

--- ntu_63134335_ThiGK/Cau2_TinhLuongNhanVien/app/src/main/java/thiGK/tinhluongnhanvien/MainActivity.java
+++ ntu_63134335_ThiGK/Cau2_TinhLuongNhanVien/app/src/main/java/thiGK/tinhluongnhanvien/MainActivity.java
@@ -1,6 +1,9 @@
 package thiGK.tinhluongnhanvien;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> dsNhanVien;//Khai bao
+    ArrayAdapter<String> adapterNhanVien;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //B1: co du lieu
+        dsNhanVien= new ArrayList<>();
+        dsNhanVien.add("Anh Khoa");
+        dsNhanVien.add("Tuan Kiet");
+        dsNhanVien.add("Trong Anh");
+        dsNhanVien.add("Quoc Hung");
+        dsNhanVien.add("Chanh Hung");
+        //B2: Tao Adapter
+        adapterNhanVien= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsNhanVien);
+        //B3 gan vao dieu khien
+        //B3.1 Tim
+         @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ListView lvNhanVien= findViewById(R.id.lvdsNhanVien);
+         //B3.2 Gan
+        lvNhanVien.setAdapter(adapterNhanVien);
+        //B4
     }
 }
