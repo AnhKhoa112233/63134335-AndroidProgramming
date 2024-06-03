65d97d1b7dec88fa75a7299b8d7b9b6a23807ff6
AnhKhoa112233 <126152545+AnhKhoa112233@users.noreply.github.com>
2024-06-03 13:48:13 +0700

    up

--- OnThiKT/app/src/main/java/ltdd_khoa/ontapthiktltdd/Cau2Fragment.java
+++ OnThiKT/app/src/main/java/ltdd_khoa/ontapthiktltdd/Cau2Fragment.java
@@ -2,6 +2,7 @@ package ltdd_khoa.ontapthiktltdd;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -27,7 +28,7 @@ public class Cau2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Bước 0. Tạo file cơ sở dữ liệu
+       /* // Bước 0. Tạo file cơ sở dữ liệu
         SQLiteDatabase db = getActivity().openOrCreateDatabase("QLSach.db", // tên file = tên DB
                 MODE_PRIVATE,  // giới hạn truy cập
                 null          // con trỏ bản ghi
@@ -57,9 +58,56 @@ public class Cau2Fragment extends Fragment {
         db.execSQL(sqlThem4);
         db.execSQL(sqlThem5);
         // Ta đóng lại để check
+        db.close(); */
+        //B1. Mở CSDL
+        SQLiteDatabase db = getActivity().openOrCreateDatabase("QLSach.db", // tên file = tên DB
+                MODE_PRIVATE,  // giới hạn truy cập
+                null          // con trỏ bản ghi
+        );
+        //B2. Thực thi câu lệnh select
+        String sqlSelect ="Select * from Books;";
+        Cursor cs = db.rawQuery(sqlSelect,null);
+        cs.moveToFirst(); // đưa con trỏ bản ghi về hàng đầu tiên
+        // B3: Đổ vào biến nào đó để xử lý
+        // 3.1. Xây dựng model/class cho bảng Books, vi dụ: Book.java
+        // 3.2. Tạo biến ArrayList<Book> dsSach;
+        View view = inflater.inflate(R.layout.fragment_cau2, container, false);
+        // Khởi tạo ListView
+        ListView listView = view.findViewById(R.id.lv_data);
+        ArrayList<Books> dsSach = new ArrayList<Books>();
+        //3.3. Duyệt qua lần lượt từng bản ghi và thêm vào danhSach
+
+        while (cs.moveToNext()) // còn bản ghi để chuyển tới
+        {
+            // Lấy dữ liệu từng côột ở dòng hiện tại
+            int idSach = cs.getInt(0);  // lấy dữ liệu ở côt 0, kiểu int
+            String tenSach = cs.getString(1);
+            int soTrang = cs.getInt(2);
+            float gia = cs.getFloat(3);
+            String mota = cs.getString(4);
+            // Tạo một đối tượng sách và thêm vào danh sách
+            Books b = new Books(idSach,tenSach,soTrang,gia,mota);
+            dsSach.add(b);
+        }
+        //B4, Hiện lên listview, recylerview,..
+        // để cho nhanh, ở đây thầy chỉ hiện tên sách
+        ArrayList<String> dsTenSach = new ArrayList<String>();
+        for (int i=0; i<dsSach.size(); i++ )
+            dsTenSach.add(dsSach.get(i).getBookName());
+        // Phần việc hiện lên là của SV
+        // Đóng cursor và cơ sở dữ liệu
+        cs.close();
         db.close();
+
+        // Tạo ArrayAdapter để liên kết tên sách với ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, dsTenSach);
+
+        // Gán adapter cho ListView
+        listView.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau2, container, false);
+
+        return view;
     }
 
 }
